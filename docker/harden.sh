19e5c32f511a476491d8defd2fa5700fbf204f4d
ssiddiqu <salmansiddiquimailbox@gmail.com>
2021-03-01 14:28:49 +0100

    [VOL-3883] WIP: DO NOT MERGE Harden voltha docker images
    Change-Id: I931b7ef0e65e8357e78dde0da045359fd611620e

---
+++ docker/harden.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#
+# Copyright 2021 The Open Networking Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# fail if a command fails
+set -e
+set -o pipefail
+
+# ensure we only use apk repositories over HTTPS (altough APK contain an embedded signature)
+echo "https://ftp.halifax.rwth-aachen.de/alpine/v$(cut -d . -f 1,2 < /etc/alpine-release)/main" > /etc/apk/repositories \
+	&& echo "https://ftp.halifax.rwth-aachen.de/alpine/v$(cut -d . -f 1,2 < /etc/alpine-release)/community" >> /etc/apk/repositories
+
+# Update base system
+apk update && apk upgrade
+apk add --no-cache curl ca-certificates
+
+# remove apk package manager after update
+find / -type f -iname '*apk*' -xdev -delete
+find / -type d -iname '*apk*' -print0 -xdev | xargs -0 rm -r --
+
+# Remove existing crontabs, if any.
+rm -fr /var/spool/cron \
+  && rm -fr /etc/crontabs \
+  && rm -fr /etc/periodic
+
+# Remove all but a handful of admin commands.
+find /sbin /usr/sbin \
+  ! -type d -a ! -name apk -a ! -name ln \
+  -delete
+
+# Remove world-writeable permissions except for /tmp/
+find / -xdev -type d -perm +0002 -exec chmod o-w {} + \
+  && find / -xdev -type f -perm +0002 -exec chmod o-w {} + \
+  && chmod 777 /tmp/ \
+  && chown $APP_USER:root /tmp/
+
+# Remove unnecessary accounts, excluding current app user and root
+sed -i -r "/^($APP_USER|root|nobody)/!d" /etc/group \
+  && sed -i -r "/^($APP_USER|root|nobody)/!d" /etc/passwd
+
+# Remove interactive login shell for everybody
+sed -i -r 's#^(.*):[^:]*$#\1:/sbin/nologin#' /etc/passwd
+
+# Disable password login for everybody
+while IFS=: read -r username _; do passwd -l "$username"; done < /etc/passwd || true
+
+# Remove temp shadow,passwd,group
+find /bin /etc /lib /sbin /usr -xdev -type f -regex '.*-$' -exec rm -f {} +
+
+# Ensure system dirs are owned by root and not writable by anybody else.
+find /bin /etc /lib /sbin /usr -xdev -type d \
+  -exec chown root:root {} \; \
+  -exec chmod 0755 {} \;
+
+# Remove suid & sgid files
+find /bin /etc /lib /sbin /usr -xdev -type f -a \( -perm +4000 -o -perm +2000 \) -delete
+
+# Remove dangerous commands
+find /bin /etc /lib /sbin /usr -xdev \( \
+  -iname hexdump -o \
+  -iname chgrp -o \
+  -iname ln -o \
+  -iname od -o \
+  -iname strings -o \
+  -iname su -o \
+  -iname sudo \
+  \) -delete
+
+# Remove init scripts since we do not use them.
+rm -fr /etc/init.d /lib/rc /etc/conf.d /etc/inittab /etc/runlevels /etc/rc.conf /etc/logrotate.d
+
+# Remove kernel tunables
+rm -fr /etc/sysctl* /etc/modprobe.d /etc/modules /etc/mdev.conf /etc/acpi
+
+# Remove root home dir
+rm -fr /root
+
+# Remove fstab
+rm -f /etc/fstab
+
+# chown all app files
+chown $APP_USER:$APP_USER -R $APP_DIR
+
+# remove chown after use (where chown is a symlink to busybox)
+find / -type l -iname 'chown' -xdev -delete
+
+# Remove any symlinks that we broke during previous steps
+find /bin /etc /lib /sbin /usr -xdev -type l -exec test ! -e {} \; -delete
+
+# Finally remove the files
+rm "$0"
