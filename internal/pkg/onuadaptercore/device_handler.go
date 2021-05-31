283e723cc21998e9d501c007e4767a61ef2e5419
ozgecanetsia <ozge.ayaz@netsia.com>
2021-05-31 17:42:09 +0300

    VOL-4140 Added new field into ONU_ACTIVATED_EVENT for TT workflow.
    Change-Id: I34c79b64e8c997e33d983f98d6c58513c146a443

--- internal/pkg/onuadaptercore/device_handler.go
+++ internal/pkg/onuadaptercore/device_handler.go
@@ -2471,8 +2471,20 @@ func (dh *deviceHandler) sendOnuOperStateEvent(ctx context.Context, aOperState v
 	eventContext["olt-serial-number"] = oltSerialNumber
 	eventContext["device-id"] = aDeviceID
 	eventContext["registration-id"] = aDeviceID //py: string(device_id)??
-	logger.Debugw(ctx, "prepare ONU_ACTIVATED event",
-		log.Fields{"device-id": aDeviceID, "EventContext": eventContext})
+	eventContext["num-of-unis"] = strconv.Itoa(len(dh.uniEntityMap))
+	eventContext["registration_id"] = aDeviceID //py: string(device_id)??
+	if deviceEntry := dh.getOnuDeviceEntry(ctx, false); deviceEntry != nil {
+		eventContext["equipment-id"] = deviceEntry.sOnuPersistentData.PersEquipmentID
+		eventContext["software-version"] = deviceEntry.onuSwImageIndications.activeEntityEntry.version
+		eventContext["vendor"] = deviceEntry.sOnuPersistentData.PersVendorID
+		eventContext["inactive-software-version"] = deviceEntry.onuSwImageIndications.inactiveEntityEntry.version
+		logger.Debugw(ctx, "prepare ONU_ACTIVATED event",
+			log.Fields{"device-id": aDeviceID, "EventContext": eventContext})
+	} else {
+		logger.Errorw(ctx, "Failed to fetch device-entry. ONU_ACTIVATED event is not sent",
+			log.Fields{"device-id": aDeviceID})
+		return
+	}
 
 	/* Populating device event body */
 	de.Context = eventContext
