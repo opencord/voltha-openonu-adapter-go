d4d0bd6c4cf94ce472b3590d612abd64dc866601
Holger Hildebrandt <holger.hildebrandt@adtran.com>
2021-08-19 06:51:21 +0000

    [VOL-3380] Functional area specific logging
    Change-Id: I67414da013d8fc82827fcdb69d4f8a34040625d3

--- cmd/openonu-adapter/main.go
+++ cmd/openonu-adapter/main.go
@@ -43,7 +43,7 @@ import (
 	"github.com/opencord/voltha-protos/v4/go/voltha"
 
 	"github.com/opencord/voltha-openonu-adapter-go/internal/pkg/config"
-	ac "github.com/opencord/voltha-openonu-adapter-go/internal/pkg/onuadaptercore"
+	ac "github.com/opencord/voltha-openonu-adapter-go/internal/pkg/core"
 )
 
 type adapter struct {
