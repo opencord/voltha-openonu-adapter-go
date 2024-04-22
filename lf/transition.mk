85dcc6d5d0b2b63e423710d12654f8750a478f23
Joey Armstrong <jarmstrong@linuxfoundation.org>
2024-04-22 15:59:02 -0400

    [VOL-5316] - Add support for tool pre-commit and repo:onf-make
    requirements.txt
    .pre-commit-config.yaml
    -----------------------
      o Install python module pre-commit.
      o Add skeleton config file for pre-commit use.
    
    Makefile
    .gitmodules
    lf/README.md
    lf/config.mk
    lf/include.mk
    lf/local/include.mk
    lf/onf-make
    lf/transition.mk
    -------------------
      o Add repo:onf-make as a git submodule.
      o Currently unused but begin adding scaffolding to transition away from
        use of static makefiles stored locally in the repository to a set of
        centrally maintained makefiles.
    
    Signed-off-by: Joey Armstrong <jarmstrong@linuxfoundation.org>
    Change-Id: I26b326184f19e89d5c443853aeedd7d1c8082d6d

---
+++ lf/transition.mk
@@ -0,0 +1,49 @@
+# -*- makefile -*-
+# -----------------------------------------------------------------------
+# Copyright 2017-2024 Open Networking Foundation Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2017-2024 Open Networking Foundation Contributors
+# SPDX-License-Identifier: Apache-2.0
+# -----------------------------------------------------------------------
+
+$(if $(DEBUG),$(warning ENTER))
+
+##-------------------##
+##---]  GLOBALS  [---##
+##-------------------##
+
+##--------------------##
+##---]  INCLUDES  [---##
+##--------------------##
+TOP             ?= .
+MAKEDIR         ?= $(TOP)/makefiles
+
+legacy-mk       ?= $(TOP)/makefiles
+sandbox-root    ?= $(abspath $(TOP))
+onf-mk-dir      ?= $(sandbox-root)/lf/onf-make/makefiles
+
+include $(onf-mk-dir)/consts.mk
+include $(onf-mk-dir)/virtualenv/include.mk
+
+##--------------------##
+##---]  INCLUDES  [---##
+##--------------------##
+include $(MAKEDIR)/include.mk
+
+ifdef LOCAL_LINT
+  include $(MAKEDIR)/lint/golang/sca.mk
+endif
+
+# [EOF]
