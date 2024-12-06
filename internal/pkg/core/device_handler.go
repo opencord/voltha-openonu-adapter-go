b49183f8684ad1365ff7cb237c7d34ba6175f470
Sridhar Ravindra <sridhar.ravindra@radisys.com>
2024-12-06 10:40:54 +0530

    [VOL-5384] - openonu adapter crash during delete device
    Change-Id: I37e9d0ff6e8ef35fe8a4e930dccc9bffb9ad28ae
    Signed-off-by: Sridhar Ravindra <sridhar.ravindra@radisys.com>

--- internal/pkg/core/device_handler.go
+++ internal/pkg/core/device_handler.go
@@ -4105,7 +4105,10 @@ func (dh *deviceHandler) GetFlowMonitoringIsRunning(uniID uint8) bool {
 	defer dh.mutexFlowMonitoringRoutineFlag.RUnlock()
 	logger.Debugw(context.Background(), "get-flow-monitoring-routine",
 		log.Fields{"device-id": dh.device.Id, "isFlowMonitoringRoutineActive": dh.isFlowMonitoringRoutineActive})
-	return dh.isFlowMonitoringRoutineActive[uniID]
+	if len(dh.isFlowMonitoringRoutineActive) != 0 {
+		return dh.isFlowMonitoringRoutineActive[uniID]
+	}
+	return false
 }
 
 func (dh *deviceHandler) StartReconciling(ctx context.Context, skipOnuConfig bool) {
